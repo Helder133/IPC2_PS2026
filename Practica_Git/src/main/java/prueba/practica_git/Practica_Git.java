a4bc073843258e9d225cecb52061be7ef9d63923
Helder Joel Juárez González <hrljuar@gmail.com>
2026-02-03 20:12:14 -0600

    Add files via upload
    Practica con git

---
+++ Practica_Git/src/main/java/prueba/practica_git/Practica_Git.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package prueba.practica_git;
+
+/**
+ *
+ * @author helder
+ */
+public class Practica_Git {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Practica de git xD");
+        System.out.println("Hello World!");
+        
+    }
+}
